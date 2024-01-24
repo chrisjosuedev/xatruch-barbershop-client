020ca4bfdb32e67af3027421dcd9c7a1583e25e9
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2024-01-23 22:40:30 -0600

    NavBar Issue Fixed

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -1,22 +1,21 @@
-import { AppLayout } from "../../app/layout/AppLayout";
+import { AppLayout } from '../../app/layout/AppLayout'
 
 export const AuthLayout = ({ children }) => {
   return (
-    <AppLayout>
-      <div className="container-fluid">
-        <div className="row header-container mt-4 p-4">
-          <div className="col-md-6 mt-4">
+    <AppLayout isLanding={false}>
+      <div className='container-fluid'>
+        <div className='row header-container mt-4 p-4'>
+          <div className='col-md-6 mt-4'>
             <h1>A tan solo pocos clicks..</h1>
-            <p className="header-text">
-              {" "}
-              Si tienes una cuenta, inicia sesión para agendar una cita. Si no
-              tienes, puedes registrarte de manera gratuita para conectar con
-              nosotros.{" "}
+            <p className='header-text'>
+              {' '}
+              Si tienes una cuenta, inicia sesión para agendar una cita. Si no tienes, puedes
+              registrarte de manera gratuita para conectar con nosotros.{' '}
             </p>
           </div>
-          <div className="col-md-6">{children}</div>
+          <div className='col-md-6'>{children}</div>
         </div>
       </div>
     </AppLayout>
-  );
-};
+  )
+}
