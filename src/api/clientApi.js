d34832318839a1017de58458ac255ca46d76c125
Cristhian J. Martinez Lara <cristhian.martinezlara@gmail.com>
2024-01-31 17:09:48 -0600

    API URI & Vercel config file

--- src/api/clientApi.js
+++ src/api/clientApi.js
@@ -1,23 +1,23 @@
-import axios from "axios";
-import { getEnvVariables } from "../helpers";
+import axios from 'axios'
+import { getEnvVariables } from '../helpers'
 
 // prod
-const { VITE_API_URL } = getEnvVariables();
+const { VITE_API_URL } = getEnvVariables()
 
 const xatruchBarberApi = axios.create({
-  baseURL: "/api",
-});
+  baseURL: VITE_API_URL,
+})
 
 // Interceptors
 xatruchBarberApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
   if (token) {
     config.headers = {
       ...config.headers,
       Authorization: `Bearer ${token}`,
-    };
+    }
   }
-  return config;
-});
+  return config
+})
 
-export default xatruchBarberApi;
+export default xatruchBarberApi
